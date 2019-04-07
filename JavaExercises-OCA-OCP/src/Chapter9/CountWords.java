2696b1312b8f46bed7f070e5029ef8ce75a7ca1a
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-04-07 20:33:50 +0200

    Method countWords changed to another version.

--- JavaExercises-OCA-OCP/src/Chapter9/CountWords.java
+++ JavaExercises-OCA-OCP/src/Chapter9/CountWords.java
@@ -1,30 +1,35 @@
 package Chapter9;
-import javax.swing.JOptionPane;
 
 public class CountWords {
 
-	public static int countWord(String source, String searchWord) {
-		
-		source = source.toLowerCase();
-		searchWord = searchWord.toLowerCase();
-		
-		int count = 0;
-		for (int i = 0; i < source.length(); i++) {
-			if (i == source.indexOf(searchWord, i)) {
-				System.out.println("ocurred at index " + i);
-				count++;
-			}
-		}
-		return count;
-	}
+    public static int countWords(String source, String searched) {
+
+        source = source.toLowerCase();
+        searched = searched.toLowerCase();
+
+        int index = 0;
+        int count = 0;
+
+        while (index != -1) {
+            index = source.indexOf(searched, index);
+            if(index != -1) {
+                count ++;
+            }
+            if(index == -1) {
+                break;
+            }
+            if(index < source.length() - 1)
+                index++;
+        }
+        return count;
+    }
 	
-	public static void main(String[] args) {
+    public static void main(String args[]) {
 
-		String source = "Now let's go to do something. Let me explain it first.";
-		String searchWord = JOptionPane.showInputDialog("write word to serch for:");
-		System.out.println(countWord(source, searchWord));
-		
-	}
+        String sourceString = "Now let's go to do something. Let me explain it first.";
+        String word = "let";
 
+        int occurrenceCount = CountWords.countWords(sourceString, word);
+        System.out.println(word + " exists " + occurrenceCount + " times inside " + sourceString);
+    }
 }
-//
