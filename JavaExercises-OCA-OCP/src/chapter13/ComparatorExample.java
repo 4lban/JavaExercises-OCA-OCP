509c264dcf7fa195922d56d3cfe6185e87fa0de0
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-04-13 15:43:40 +0200

    Binary Search

--- JavaExercises-OCA-OCP/src/chapter13/ComparatorExample.java
+++ JavaExercises-OCA-OCP/src/chapter13/ComparatorExample.java
@@ -15,10 +15,18 @@ public class ComparatorExample {
 		employeeList.add(new Employee(34, 1000.0, "01/01/2019"));
 		employeeList.add(new Employee(30, 3000.0, "01/01/2020"));
 		
-		Collections.sort(employeeList, new EntranceDateComparator());
+		Collections.sort(employeeList, new AgeComparator());
 		
 		for (Employee employee : employeeList) {
 			System.out.println(employee);
+		}
+		
+		System.out.println("\n***********Binary Search********\n");
+		
+		int index = Collections.binarySearch(employeeList, new Employee(30, 0.0, "09/09/2099"), new AgeComparator());
+		
+		if (index >= 0) {
+			System.out.println("Found at index: " + index + "\n"+ employeeList.get(index));
 		}
 		
 	}
